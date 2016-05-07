00f4f3f0882ad24ce182e84c7ab3da4a4db465c2
Chandu Kavar <chanduk@thoughtworks.com>
2016-05-07 22:52:44 +0530

    Chandu: seperate out products and payment json

--- utils.js
+++ utils.js
@@ -1,4 +1,9 @@
 var api = require("./api.js");
+var jsonfile = require('jsonfile');
+var conf = require('config');
+
+var paymentDataPath = conf.get('paymentDataPath');
+var productsPath = conf.get('productsPath');
 var apiInstance = new api();
 
 var utils = function (){
@@ -10,62 +15,19 @@ var utils = function (){
             };
         apiInstance.send(sender, messageData);
    };
+    
    self.sendPaymentMessage = function sendPaymentMessage(sender) {
-        messageData = {
-            "attachment":{
-                "type":"template",
-                "payload":{
-                    "template_type":"button",
-                    "text":"Your payment of 10000Rs has been processed",
-                    "buttons":[
-                        {
-                            "type":"postback",
-                            "title":"View Payment",
-                            "payload":"USER_DEFINED_PAYLOAD"
-                        }
-                    ]
-                }
-            }
-        };
-        apiInstance.send(sender, messageData);
+       var paymentJson = paymentDataPath + "payment.json";
+       jsonfile.readFile(paymentJson, function (err, jsonObj) {
+           apiInstance.send(sender, jsonObj);
+       });
    };
-   self.sendProducts = function sendGenericMessage(sender) {
-        messageData = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Intex Cloud Cube",
-                        "subtitle": "Lets make the world better",
-                        "image_url": "http://www.themobileindian.com/images/new_launches/2015/05/5503/Cloud-M5-II.jpg",
-                        "buttons": [{
-                            "type": "web_url",
-                            "url": "http://www.flipkart.com/intex-cloud-cube/p/itmed7g38hqrz2n5?pid=MOBED7G3G2JJXEZM&al=DIhLapwSxrFFtYD5KTmlwcldugMWZuE7Qdj0IGOOVqsLCvvHEKSlsPIH4ZjRxAjF8TeOIlTNllg%3D&ref=L%3A3898321916473331522&srno=b_1",
-                            "title": "Specifications"
-                        }, {
-                            "type": "postback",
-                            "title": "Buy",
-                            "payload": "Intex-Cloud-Cube"
-                        }]
-                    },{
-                        "title": "Apple iPhone 6S",
-                        "subtitle": "Think different.",
-                        "image_url": "http://cdn.images.express.co.uk/img/dynamic/59/590x/Apple-iPhone-6s-Apple-iPhone-6s-Review-Woman-Gets-iPhone-6s-Early-ATT-iPhone-6s-Smartphone-iPhone-Smartphone-Review-Apple-ATT-iP-607026.jpg",
-                        "buttons": [{
-                            "type": "web_url",
-                            "url": "http://www.flipkart.com/apple-iphone-6s/p/itmebysgqqaranas?pid=MOBEBY3VG2Z2HVGJ&al=DIhLapwSxrEd3KiFyEoK98ldugMWZuE7Qdj0IGOOVquM9KAOUAaCfKdCtfhV9Mb1SoqZDgVoaEc%3D&ref=L%3A-5957458462133632693&srno=b_3&findingMethod=Menu&otracker=nmenu_sub_electronics_0_Apple",
-                            "title": "Specifications"
-                        },{
-                            "type": "postback",
-                            "title": "Buy",
-                            "payload": "Apple-iPhone-6S"
-                        }]
-                    }]
-                }
-            }
-        };
-        apiInstance.send(sender, messageData);
+    
+   self.sendProducts = function sendProducts(sender) {
+       var productJson = productsPath + "products.json";
+       jsonfile.readFile(productJson, function (err, jsonObj) {
+           apiInstance.send(sender, jsonObj);
+       });
     };
 };
 
